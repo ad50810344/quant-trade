7ae14963dd20bda88e81be0420212fa2e010a963
du <104284753+15221624883@users.noreply.github.com>
2022-05-06 15:52:47 +0800

    Update spreadPlot.py

--- spreadPlot.py
+++ spreadPlot.py
@@ -116,6 +116,39 @@ class analyse:
         dfSortOrders = dfOrders.sort_values("insert_time", ignore_index= True)
         dfSortOrders.to_csv("orders.csv")
         pass
+def ui(api):
+    klines1 = api.get_tick_serial("SHFE.ag2207", data_length = 1800)
+    klines2 = api.get_tick_serial("SHFE.ag2206", data_length = 1800)
+
+    plt.ion()
+    # Create a figure and a set of subplots.
+    figure, ax = plt.subplots()
+    # return AxesImage object for using.
+    lines, = ax.plot([], [])
+    ax.set_autoscaley_on(True)
+    # ax.set_xlim(min_x, max_x)
+    ax.grid()
+    while True:
+        api.wait_update() 
+        spread = np.array(list(klines1.ask_price1)) - np.array(list(klines2.ask_price1))
+        xdata = np.arange(1800)
+        ydata = spread.tolist()
+        # print(ydata)
+        # print(len(ydata))
+        # print(klines1["close"].tolist())
+        # print(len(klines1["close"].tolist()))
+        # update x, y data
+        lines.set_xdata(xdata)
+        lines.set_ydata(ydata)
+        #Need both of these in order to rescale
+        ax.relim()
+        ax.autoscale_view()
+        # draw and flush the figure .
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+
+        # print(klines1["open"])
+        sleep(1)
 
 def main():
     simnowAccout = TqAccount(broker_id= "simnow", account_id= "138411", password= "15221shuai")
